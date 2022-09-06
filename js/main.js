f6628668b839cbf392dc10cdc2940f65dc5c741b
OleksiiRemez <ilbereth1988@gmail.com>
2022-09-06 19:51:30 +0300

    add next and prev buttons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const slides = document.querySelectorAll(".slide");
-const pauseButton = document.querySelectorAll("#pause");
+const pauseButton = document.querySelector("#pause");
+const prevButton = document.querySelector("#prev");
+const nextButton = document.querySelector("#next");
 
 let currentSlide = 0;
 let isPlaying = true;
@@ -7,24 +9,42 @@ let timerID = null;
 const interval = 1000;
 const slidesLenght = slides.length;
 
-function nextSlide() {
+function goToSlide(n) {
+  console.log(n);
   slides[currentSlide].classList.toggle("active");
   currentSlide = (currentSlide + 1) % slidesLenght;
   slides[currentSlide].classList.toggle("active");
 }
 
+function prevSlide() {
+  goToSlide(currentSlide - 1);
+}
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
 timerID = setInterval(nextSlide, interval);
+
+function pauseSlideShow() {
+  clearInterval(timerID);
+  isPlaying = false;
+  pauseButton.innerHTML = "Play";
+}
+
+function playSlideShow() {
+  timerID = setInterval(nextSlide, interval);
+  isPlaying = true;
+  pauseButton.innerHTML = "Pause";
+}
 
 function pauseHandler() {
   if (isPlaying) {
-    clearInterval(timerID);
-    isPlaying = false;
-    pauseButton.innerHTML = "Play";
+    pauseSlideShow();
   } else {
-    timerID = setInterval(nextSlide, interval);
-    isPlaying = true;
-    pauseButton.innerHTML = "Pause";
+    playSlideShow();
   }
 }
 
 pauseButton.addEventListener("click", pauseHandler);
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", nextSlide);
